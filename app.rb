de11aee4021b11355ef463951d74b86de0bcf2ba
Lucas Nseyep <lucas.nseyep@gmail.com>
2023-06-03 12:27:48 +0100

    Sinatra classic app structure

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require "sinatra"
-require "sinatra/reloader" if development?
+# require "sinatra/reloader" if development?
 
 get "/" do
   "Hello world!"
 end
+
+# get "/about" do
+#   erb :about
+# end
+
+# require 'sinatra/base'
+
+# class App < Sinatra::Base
+#   get "/" do
+#     "hello world"
+#   end
+# end
